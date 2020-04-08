a6d05244a54e7cd188c5be32d7919dc3e8bcecdc
BlackSemkaJ <nekrashevich.jobs@gmail.com>
2020-04-08 07:35:59 +0300

    Добавлен вывод Users в таблицу

--- project-vue/src/App.vue
+++ project-vue/src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <v-app>
+    <v-navigation-drawer app>
+      
+    </v-navigation-drawer>
+
+    <v-app-bar  class="white" app>
+      <Header/>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
     <v-content>
-      <Home/>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <Home/>
+        <router-view></router-view>
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import Home from './components/Home';
+import Header from './components/Header';
 
 export default {
   name: 'App',
 
   components: {
     Home,
+    Header,
   },
 
   data: () => ({
